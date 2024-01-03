155b87e11059d7437a909bbf6ab09c29b94db8b6
m1ch433l <zabkamichael06@gmail.com>
2024-01-03 16:34:59 +0100

    fix: config fix

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 Config = {
     -- Set the keybind to stop carrying a player
-    stopCarryKeybind = "G" -- Default keybind which can players change in settings
-    leaveTrunkKeybind = "E" -- Default keybind which can players change in settings
-    showPlayerInTrunk = false -- Option to show player hiden in trunk
-    allowCarryAsCommand = false -- This option will allow /carry command. Target will be working too
+    stopCarryKeybind = "G", -- Default keybind which can players change in settings
+    leaveTrunkKeybind = "E", -- Default keybind which can players change in settings
+    showPlayerInTrunk = false, -- Option to show player hiden in trunk
+    allowCarryAsCommand = false, -- This option will allow /carry command. Target will be working too
 }
