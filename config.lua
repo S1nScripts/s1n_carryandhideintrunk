d39adcd79b3f811ba7c38c08ae22f303f42a6775
m1ch433l <153664630+m1ch433l@users.noreply.github.com>
2024-01-14 13:01:40 +0100

    style: added export explanation

--- config.lua
+++ config.lua
@@ -6,3 +6,9 @@ Config = {
     allowCarryAsCommand = false, -- This option will allow /carry command. Target will be working too
     allowBlackout = false, -- This function allows player screen to be blackouted when trunk is closed if trunk opens player will see again.
 }
+--[[
+    EXPORTS
+
+    exports['s1n_carryandhideintrunk']:isPedOnCarry() - returns true if player is carried and false if not
+
+]]--
